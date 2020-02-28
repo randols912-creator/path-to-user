e4a4172e8e5775b193ece667f108411faa0739b3
Vladimir Bredikhin <vladimir.bredikhin@broscorp.net>
2020-02-28 17:10:07 +0200

    Daily backup

--- geni-app/src/app/app.constants.ts
+++ geni-app/src/app/app.constants.ts
@@ -3,3 +3,11 @@ export const welcomePath = 'welcome';
 
 export const homeUrl = `/${homePath}`;
 export const welcomeUrl = `/${welcomePath}`;
+
+export const welcomePhotos = [
+  'assets/img/photos/photo_2.png',
+  'assets/img/photos/photo_1.png',
+  'assets/img/photos/photo_3.png',
+  'assets/img/photos/photo_1.png',
+  'assets/img/photos/photo_2.png',
+];
--- geni-app/src/app/components/containers/welcome/welcome.component.ts
+++ geni-app/src/app/components/containers/welcome/welcome.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { welcomePhotos } from 'src/app/app.constants';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
+  photos = welcomePhotos;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
