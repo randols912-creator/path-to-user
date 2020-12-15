e598eab3a0982065328db63a3da2075617a58e0b
Vladimir Bredikhin <vladimir.bredikhin@broscorp.net>
2020-12-15 14:26:36 +0200

    Restore p2p button, add some fixes
    * fix p2p modal styling
    * add listener to close modal on route change

--- src/app/src/app/services/modal.service.ts
+++ src/app/src/app/services/modal.service.ts
@@ -1,4 +1,6 @@
 import { ComponentFactoryResolver, Injectable } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { ModalRefDirective } from '../directives/modal-ref.directive';
 
 @Injectable({
@@ -8,7 +10,18 @@ export class ModalService {
   public modalRef: ModalRefDirective;
   private modal: any;
 
-  constructor(private resolver: ComponentFactoryResolver) {}
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private router: Router
+  ) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe(() => {
+        if (this.modal) {
+          this.modal.instance.closeModal.emit();
+        }
+      });
+  }
 
   open(modalComponent: any) {
     const modalFactory = this.resolver.resolveComponentFactory<any>(
