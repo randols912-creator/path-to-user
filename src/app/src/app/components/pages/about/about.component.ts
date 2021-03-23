0b2ccda8d5cb36a2a72ab78b19c83ec720db6bd8
shasha79 <alexander.raginsky@gmail.com>
2021-03-23 21:04:50 +0100

    Updates

--- src/app/src/app/components/pages/about/about.component.ts
+++ src/app/src/app/components/pages/about/about.component.ts
@@ -12,9 +12,9 @@ export class AboutComponent {
   share() {
     try {
       window.navigator['share']({
-        title: 'Geni App | Museum of the Jewish People',
+        title: 'ANU Family | Museum of the Jewish People',
         text:
-          'The Museum of the Jewish People at Beit Hatfutsot is more than a Museum. This unique global institution tells the ongoing and extraordinary story of the Jewish people.',
+          'ANU - The Museum of the Jewish People at Beit Hatfutsot is more than a Museum. This unique global institution tells the ongoing and extraordinary story of the Jewish people.',
         url: window.location.href,
       });
     } catch (err) {
