2332b6e32e400c35b4f7d83925a538b321f3a76f
neel <neeldeveloper97@gmail.com>
2022-03-25 19:26:00 +0530

    GENI-18

--- src/app/src/app/components/pages/settings/filters/filters.component.ts
+++ src/app/src/app/components/pages/settings/filters/filters.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { faSlash } from '@fortawesome/free-solid-svg-icons';
 import { getColor, Theme } from 'src/app/model/Theme';
 import { SettingsService } from 'src/app/services/settings.service';
 // import { AccordionModule } from 'ngx-bootstrap/accordion';
@@ -15,64 +16,149 @@ export class FiltersComponent implements OnInit {
   filterForm: FormGroup;
   oneAtATime = true;
   male_femaleOptions = ['Male', 'Female',];
-
-  birth_country = ['Afghanistan','Albania','Algeria','Angola','Argentina','Armenia','Australia','Austria','Azerbaijan','Belarus','Belgium',
-  'Belize','Bolivia','Brazil','Bulgaria','Canada'];
+  selectedItemsList = [];
+  selectedmuseumList = [];
+  ischeckedradio: boolean = false;
+  isCheckedcountry: boolean = false;
+  isCheckedmuseum: boolean = false;
+  isCheckrdprofession: boolean = false;
+  selectedprofessionsList = [];
+  birth_country = [
+    { name: 'Afghanistan', isChecked: false },
+    { name: 'Albania', isChecked: false },
+    { name: 'Algeria', isChecked: false },
+    { name: 'Angola', isChecked: false },
+    { name: 'Argentina', isChecked: false },
+    { name: 'Armenia', isChecked: false },
+    { name: 'Australia', isChecked: false },
+    { name: 'Austria', isChecked: false },
+    { name: 'Azerbaijan', isChecked: false },
+    { name: 'Belarus', isChecked: false },
+    { name: 'Belgium', isChecked: false },
+    { name: 'Belize', isChecked: false },
+    { name: 'Brazil', isChecked: false },
+    { name: 'Bulgaria', isChecked: false },
+    { name: 'Canada', isChecked: false }];
 
   place_at_museum = [
-    'Ground floor',
-    'first floor',
-    'Second floor',
-    'Third floor'];
+    { place_museum_name: 'Ground floor', isChecked: false },
+    { place_museum_name: 'first floor', isChecked: false },
+    { place_museum_name: 'Second floor', isChecked: false },
+    { place_museum_name: 'Third floor', isChecked: false }];
 
-  profession = [
-    { id: 1, profession_name: 'Humanities & Religion' },
-    { id: 2, profession_name: 'Science' },
-    { id: 3, profession_name: 'Diplomacy' },
-    { id: 4, profession_name: 'Law' },
-    { id: 5, profession_name: 'Economics' },
-    { id: 6, profession_name: 'The Holocaust' },
-    { id: 7, profession_name: 'Press & Media' },
-    { id: 8, profession_name: 'Military' },
-    { id: 9, profession_name: 'Literature & Poetry' },
-    { id: 10, profession_name: 'Plastic Art' },
-    { id: 11, profession_name: 'Classical Music' },
-    { id: 12, profession_name: 'Popular Music' },
-    { id: 13, profession_name: 'Architecture' },
-    { id: 14, profession_name: 'Theatre' },
-    { id: 15, profession_name: 'Cinema & Television' },
-    { id: 16, profession_name: 'Photography & Comics' },
-    { id: 17, profession_name: 'Humor' },
-    { id: 18, profession_name: 'Sports' },
-    { id: 19, profession_name: 'Other' }];
+  professions = [
+    { isChecked: false, profession_name: 'Humanities & Religion' },
+    { isChecked: false, profession_name: 'Science' },
+    { isChecked: false, profession_name: 'Diplomacy' },
+    { isChecked: false, profession_name: 'Law' },
+    { isChecked: false, profession_name: 'Economics' },
+    { isChecked: false, profession_name: 'The Holocaust' },
+    { isChecked: false, profession_name: 'Press & Media' },
+    { isChecked: false, profession_name: 'Military' },
+    { isChecked: false, profession_name: 'Literature & Poetry' },
+    { isChecked: false, profession_name: 'Plastic Art' },
+    { isChecked: false, profession_name: 'Classical Music' },
+    { isChecked: false, profession_name: 'Popular Music' },
+    { isChecked: false, profession_name: 'Architecture' },
+    { isChecked: false, profession_name: 'Theatre' },
+    { isChecked: false, profession_name: 'Cinema & Television' },
+    { isChecked: false, profession_name: 'Photography & Comics' },
+    { isChecked: false, profession_name: 'Humor' },
+    { isChecked: false, profession_name: 'Sports' },
+    { isChecked: false, profession_name: 'Other' }];
+  country: any;
+  museum: any;
+  profession_name: any;
 
   constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
     this.filterForm = new FormGroup({
       filterOrder: new FormControl(this.settingsService.getSortOrder()),
-      country: new FormControl(this.settingsService.getSortOrder())
+      country: new FormControl(this.settingsService.getSortOrder()),
+      museum: new FormControl(this.settingsService.getSortOrder()),
+      profession_name: new FormControl(this.settingsService.getSortOrder())
     });
     this.filterForm.valueChanges.subscribe((value) => {
       this.settingsService.setSortOrder(value.filterOrder);
     });
     this.filterForm.valueChanges.subscribe((value) => {
       this.settingsService.setSortOrder(value.country);
-      console.log(value.country)
     });
-
-  }
-
-  onchange1() {
-    console.log('change ==> ', this.filterForm.value)
+    this.filterForm.valueChanges.subscribe((value) => {
+      this.settingsService.setSortOrder(value.museum);
+    });
+    this.filterForm.valueChanges.subscribe((value) => {
+      this.settingsService.setSortOrder(value.profession_name)
+    })
   }
 
   onchange() {
-    console.log('change ==> ', this.filterForm.value)
+    if (this.filterForm.value.filterOrder == 0 || this.filterForm.value.filterOrder == 1) {
+      this.ischeckedradio = true;
+    } else {
+      this.ischeckedradio = false;
+    }
+  }
+
+  clear() {
+    this.filterForm.value.filterOrder = '';
+    this.ischeckedradio = false;
+  }
+
+  onchangecountry() {
+    this.fetchSelectedItems();
+    this.country = this.birth_country.filter((item) => item.isChecked)
+    if (this.country.length > 0) {
+      this.isCheckedcountry = true;
+    } else {
+      this.isCheckedcountry = false;
+    }
+  }
+
+  clearcountry(){
+    this.fetchSelectedItems();
+
+  }
+
+  onchangemuseum() {
+    this.fetchSelectedItems();
+    this.museum = this.place_at_museum.filter((item) => item.isChecked);
+    if (this.museum.length > 0) {
+      this.isCheckedmuseum = true;
+    } else {
+      this.isCheckedmuseum = false;
+    }
+  }
+
+  onchangeprofession() {
+    this.fetchSelectedItems();
+    this.profession_name = this.professions.filter((item) => item.isChecked);
+    if (this.profession_name.length > 0) {
+      this.isCheckrdprofession = true;
+    } else {
+      this.isCheckrdprofession = false;
+    }
   }
 
   categoryColor(category: Theme): string {
     return getColor(category);
   }
 
+  fetchSelectedItems() {
+    if (this.filterForm.value.country) {
+      this.selectedItemsList = this.birth_country.filter((value, index) => {
+        console.log(value.isChecked)
+        return value.isChecked
+      });
+    } else if (this.filterForm.value.museum) {
+      this.selectedmuseumList = this.place_at_museum.filter((value, index) => {
+        return value.isChecked
+      });
+    } else if (this.filterForm.value.profession_name) {
+      this.selectedprofessionsList = this.professions.filter((value, index) => {
+        return value.isChecked
+      });
+    }
+  }
 }
