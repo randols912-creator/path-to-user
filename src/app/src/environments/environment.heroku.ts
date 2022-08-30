307ee0d7dc0d05dcac472b1a9548e50f10519321
shasha79 <alexander.raginsky@gmail.com>
2022-08-30 14:13:44 +0300

    Heroku: environment

---
+++ src/app/src/environments/environment.heroku.ts
@@ -0,0 +1,8 @@
+import Environment from './IEnvironment';
+
+export const environment: Environment = {
+  production: true,
+  host: 'https://path-to-user.herokuapp.com',
+  relationsServiceHost: 'https://path-to-user.herokuapp.com',
+  socketioUrl: 'https://path-to-user.herokuapp.com',
+};
